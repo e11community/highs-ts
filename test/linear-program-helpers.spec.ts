140491aca9c0d7b6523a74f80887bf17e1fbdeaf
Piers Francis <piers.brennand-francis@engineering11.com>
2024-11-14 16:43:09 -0600

    test: finish mergeInputParts test

--- test/linear-program-helpers.spec.ts
+++ test/linear-program-helpers.spec.ts
@@ -1,23 +1,31 @@
-import {ILPInputPart, mergeInputParts} from '../src/linear-program.helpers'
+import { ILPInputPart, mergeInputParts } from "../src/linear-program.helpers"
 
-describe('#mergeInputParts', () => {
-  it('should merge different input parts correctly', () => {
+describe("#mergeInputParts", () => {
+  it("should merge different input parts correctly", () => {
     const part1: ILPInputPart = {
-      constraints: ['a > 0', 'b > 0'],
+      constraints: ["a > 0", "b > 0"],
       objective: `1 a + 2 b`,
-      binaries: ['a'],
+      binaries: ["a"],
       bounds: [],
-      integers: ['b'],
+      integers: ["b"],
     }
     const part2: ILPInputPart = {
-      constraints: ['c > 0', 'd > 0'],
+      constraints: ["c > 0", "d > 0"],
       objective: `3 c + 4 d`,
-      binaries: ['c'],
+      binaries: ["c"],
       bounds: [],
-      integers: ['d'],
+      integers: ["d"],
     }
 
-    const result = mergeInputParts([part1, part2])
-    console.log(result)
+    const actual = mergeInputParts([part1, part2])
+    // We get away with the objective function because the merge happens in order
+    const expected = {
+      constraints: ["a > 0", "b > 0", "c > 0", "d > 0"],
+      objective: "1 a + 2 b + 3 c + 4 d",
+      bounds: [],
+      integers: ["b", "d"],
+      binaries: ["a", "c"],
+    }
+    expect(actual).toEqual(expected)
   })
 })
